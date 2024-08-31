5a16098aff11b7b11ad630399b4f1a25ea36d2e3
Antoine BERLAUD <antoine@MacBook-Pro-BLD.local>
2024-08-31 21:29:30 +0200

    Premier commit

---
+++ firstScript.py
@@ -0,0 +1,37 @@
+login = ""
+password = ""
+loginStatus = False
+passwordStatus = False
+
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+antoine = User("gold", "glint")
+nico = User("nico", "pass")
+adolf = User("dodolf", "ilikegerman")
+
+users = [antoine, nico, adolf]
+
+loginInput = input("Enter your login :\n")
+
+while loginStatus is False:
+    for user in users:
+        if user.login == loginInput:
+            loginStatus = True
+            userConnected = user
+    if loginStatus is False:
+        print("Wrong login")
+        loginInput = input("Enter your login :\n")
+
+passwordInput = input("Enter your password :\n")
+
+while passwordStatus is False:
+    if userConnected.password == passwordInput:
+        passwordStatus = True
+    if passwordStatus is False:
+        print("Wrong password")
+        passwordInput = input("Enter your password :\n")
+
+print("Your logged in")
