be395bed896f63e9028342d417062367aad05190
Nicolas <ni.berlaud@gmail.com>
2024-09-15 11:18:15 +0200

    Change l'instanciation des paramètres en local
    Co-authored-by: Antoine BERLAUD <antoine@MacBook-Pro-BLD.local>

--- controllers/connexionController.py
+++ controllers/connexionController.py
@@ -1,27 +1,18 @@
-from models.User import User
 from views import connexionView
 
 class ConnexionController:
-    def __init__(self):
-        self.users = [
-            User("gold", "glint"),
-            User("nico", "pass")
-        ]
-        self.loginStatus = False
-        self.passwordStatus = False
-
-    def isUser(self, loginInput, passwordInput):
+    def isUser(self, loginInput: str, passwordInput: str):
         for user in self.users:
             if user.login == loginInput:
                 if user.password == passwordInput:
                     return True
         return False
-
+    
     def login(self):
         loginInput = connexionView.getLoginInput()
         passwordInput = connexionView.getPasswordInput()
-        if self.isUser(loginInput, passwordInput):
+        if ConnexionController.isUser(self, loginInput, passwordInput):
             connexionView.printUserConnected()
         else:
             connexionView.printWrongLoginOrPassword()
-            self.login()
+            ConnexionController.login()
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from models.User import User
+from models.SCP import SCP
 from controllers.connexionController import ConnexionController
 
+class MainApp:
+    def __init__(self):
+        self.users = [
+            User("gold", "glint"),
+            User("nico", "pass")
+        ]
+        self.scps = [
+            SCP("002", "contained", "euclid", None),
+            SCP("096", "contained", "euclid", None),
+            SCP("409", "contained", "keter", None)
+        ]
+        
+    def run(self):
+        ConnexionController.login(self)
+
+
 if __name__ == "__main__":
-    controller = ConnexionController()
-    controller.login()
+    app = MainApp()
+    app.run()
